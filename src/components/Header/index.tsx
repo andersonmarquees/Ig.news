647eeb999d9b7271148a35e84e3565b8ba46a078
Anderson Marques <anderson_fisico@yahoo.com.br>
2021-08-04 18:32:57 -0300

    home page styles and html

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,16 @@
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+
 export function Header() {
   return (
-    <header>
-      <div>
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Pots</a>
+        </nav>
+        <SignInButton />
       </div>
     </header>
   );
